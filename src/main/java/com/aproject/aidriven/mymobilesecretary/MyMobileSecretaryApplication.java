719a748a89a6c999285412b3a8c5e1b9e3dd741b
AidenChen <wei310129@gmail.com>
2026-07-08 17:49:14 +0800

    format: adjust code indentation for consistency

--- src/main/java/com/aproject/aidriven/mymobilesecretary/MyMobileSecretaryApplication.java
+++ src/main/java/com/aproject/aidriven/mymobilesecretary/MyMobileSecretaryApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MyMobileSecretaryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MyMobileSecretaryApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MyMobileSecretaryApplication.class, args);
+    }
 
 }

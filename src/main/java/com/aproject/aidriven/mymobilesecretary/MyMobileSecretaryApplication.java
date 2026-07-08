38fce8855d03e6820fcea62a9b045016d149f20d
AidenChen <wei310129@gmail.com>
2026-07-08 17:06:03 +0800

    first commit

---
+++ src/main/java/com/aproject/aidriven/mymobilesecretary/MyMobileSecretaryApplication.java
@@ -0,0 +1,13 @@
+package com.aproject.aidriven.mymobilesecretary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMobileSecretaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMobileSecretaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aproject/aidriven/mymobilesecretary/MyMobileSecretaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aproject.aidriven.mymobilesecretary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMobileSecretaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
